e1353aef3164e2aef6e387244c1b8ef08c234f67
20296416 <l2029686416@qq.com>
2020-04-03 14:41:03 +0800

    打包springboot修改的文件

--- src/main/java/com/demo/SpringbootBruidApplication.java
+++ src/main/java/com/demo/SpringbootBruidApplication.java
@@ -3,13 +3,20 @@ package com.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.demo.web.dao")
-public class SpringbootBruidApplication {
+//在启动类当中加上extends SpringBootServletInitializer并重写configure方法，这是为了打包springboot项目用的。
+public class SpringbootBruidApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBruidApplication.class, args);
+	}
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(this.getClass());
 	}
 
 }
